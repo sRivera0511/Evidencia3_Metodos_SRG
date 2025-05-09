eb4581f0a5fc964e1138a2739bb64483284c1a5a
sRivera0511 <riveragsebastian14@gmail.com>
2025-05-09 15:15:51 -0700

    Realización de método para mostrar la matriz aumentada

--- src/GaussJordan.java
+++ src/GaussJordan.java
@@ -38,16 +38,19 @@ public class GaussJordan {
     System.out.println("\n------------------------------ Coeficientes para la matriz aumentada ------------------------------\n");
     
     System.out.println("A continuación, ingrese los coeficientes de la matriz aumentada:");
-    
+
     for (int i = 0; i < n; i++) {                               // Contador que toma en cuenta el número de ecuaciones.
         System.out.println("\nECUACIÓN " + (i+1) + "\n|");
         for (int j = 0; j < n; j++) {                           // Contador que toma en cuenta el número de coeficientes.
             System.out.print("|_ Coeficiente " + (j+1) + ": ");
             matriz[i][j] = sc.nextDouble();
         }
-        System.out.println("|\n|_ Término independiente: ");      // Se le solicita al usuario el término independiente.
+        System.out.print("|\n|_ Término independiente: ");      // Se le solicita al usuario el término independiente.
         matriz[i][n] = sc.nextDouble();
     }
+
+    System.out.println("\n\nMATRIZ AUMENTADA INGRESADA\n");      
+    mostrarMatriz(matriz);  // Se llama el método para mostrar la matriz aumentada.
 
     System.out.println("\n--------------------------------- Programa Finalizado v1.0 by SRG ---------------------------------\n");
 
@@ -55,4 +58,13 @@ public class GaussJordan {
 
     }
 
+    // Método para mostrar la matriz en la terminal.
+    public static void mostrarMatriz(double [][] matriz) {
+        for (double[] fila : matriz) {
+            for (double valor : fila) {
+                System.out.print("    " + valor);   // Deja un espacio entre cada valor para que no se vean pegados.
+            }
+            System.out.println();
+        }
+    }
 }
