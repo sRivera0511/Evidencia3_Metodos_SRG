ad7cb08920343283ba45cfc4a1a0d56a8a5dc094
sRivera0511 <riveragsebastian14@gmail.com>
2025-05-09 14:29:01 -0700

    Inicialización del Proyecto

---
+++ src/GaussJordan.java
@@ -0,0 +1,16 @@
+public class GaussJordan {
+
+    public static void main (String[]args) {
+    
+    System.out.println("\n------------------------------ Evidencia 3 | Métodos Númericos | SRG ------------------------------\n");
+
+    // PANTALLA INICIAL DEL PROGRAMA
+    System.out.println("Método de Gauss Jordan\nv1.0 by SRG");
+    System.out.print("\nInserte el número de ecuaciones lineales: (Ej. 3)\n> ");
+    
+
+    System.out.println("\n--------------------------------- Programa Finalizado v1.0 by SRG ---------------------------------\n");
+
+    }
+
+}
